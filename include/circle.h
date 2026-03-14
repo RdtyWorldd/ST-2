0ad72f6537b8b7e4458a8348e3c0a546e9426e23
Nikita Morozov <nickitrytry@gmail.com>
2026-03-14 13:27:09 +0300

    ST2: Исправлено форматирование файлов

--- include/circle.h
+++ include/circle.h
@@ -1,34 +1,33 @@
 // Copyright 2022 UNN-CS
 #ifndef INCLUDE_CIRCLE_H_
 #define INCLUDE_CIRCLE_H_
-#include <cstdint>
 
 #include <cmath>
 #include <stdexcept>
 
 class Circle {
-private:
-    double radius;
-    double ference;
-    double area;
-    
-    static constexpr double PI = 3.1415;
-    
-public:
-    Circle(double r);
+ private:
+  double radius;
+  double ference;
+  double area;
 
-    void setRadius(double r);
-    void setFerence(double f);
-    void setArea(double a);
+  static constexpr double PI = 3.1415;
 
-    double getRadius() const;
-    double getFerence() const;
-    double getArea() const;
-    
-private:
-    void updateFromRadius();
-    void updateFromFerence();
-    void updateFromArea();
+ public:
+  explicit Circle(double r);
+
+  void setRadius(double r);
+  void setFerence(double f);
+  void setArea(double a);
+
+  double getRadius() const;
+  double getFerence() const;
+  double getArea() const;
+
+ private:
+  void updateFromRadius();
+  void updateFromFerence();
+  void updateFromArea();
 };
 
 #endif  // INCLUDE_CIRCLE_H_
--- include/tasks.h
+++ include/tasks.h
@@ -1,8 +1,10 @@
+// Copyright 2025 UNN-CS
 #ifndef INCLUDE_TASKS_H_
 #define INCLUDE_TASKS_H_
+
 #include "circle.h"
 
 double earthEndRope();
 double calculateMaterialPrice();
 
-#endif // INCLUDE_TASKS_H_
+#endif  // INCLUDE_TASKS_H_
